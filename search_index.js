eda8ba763e3a73b6bfb7acd9e6f8ed023b1fcd45
Daniel Menendez Crespo <danielmail7@gmail.com>
2019-11-12 04:07:52 +0100

    Prepared github page

---
+++ search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#SciAlgs.jl-1",
+    "page": "Home",
+    "title": "SciAlgs.jl",
+    "category": "section",
+    "text": "Modules = [SciAlgs]"
+},
+
+]}
