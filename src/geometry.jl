74279377b0923f8aa4dec5e60edc01e2fea226d8
Daniel Menendez <Daniel.MenendezCrespo@cpfs.mpg.de>
2019-11-26 16:15:51 +0100

    Better organization

--- src/geometry.jl
+++ src/geometry.jl
@@ -1,4 +1,6 @@
 
+using LinearAlgebra
+
 function cart2polar(point)
   epsrad = 1e-7
   cost = 0.0
@@ -22,3 +24,24 @@ function cart2polar(point)
   cost, sint, cosp, sinp
 end
 
+
+# Naive algorithm
+#
+points = rand(50,3)
+
+function closest_point(points)
+   npoints = size(points,1)
+   minDist = Inf
+   closestPair = []
+   for i in 1:npoints-1
+      for j in (i+1):npoints
+         p = points[i,:]
+         q = points[j,:]
+         if norm(p-q) < minDist
+            minDist = norm(p-q)
+            closestPair = [p,q]
+         end
+      end
+   end
+   closestPair
+end
