7a1b287dd5f82b633ab9d3abb1bfa4cd806147f8
Daniel Menendez Crespo <danielmail7@gmail.com>
2020-06-26 00:09:52 +0200

    Example interfacing Fortran code

---
+++ src/interface_fortran/callfortran.jl
@@ -0,0 +1,3 @@
+
+five = ccall( (:__m_MOD_five, "fortranlib.so"), Int, () )
+println("Five = ",five)
---
+++ src/interface_fortran/fortrancode.f90
@@ -0,0 +1,7 @@
+
+module m
+  contains
+  integer function five()
+    five = 5
+  end function five
+end module m
