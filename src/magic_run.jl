e4616d4f2745bdfc54ca5e26b7cc882addd42cc4
Daniel Menendez Crespo <danielmail7@gmail.com>
2020-07-23 22:21:19 +0200

    @run "script.jl"

--- src/magic_run.jl
+++ src/magic_run.jl
@@ -1,11 +1,18 @@
 
-# Run Julia files from the REPL, e.g.
-# julia> @run file.jl      (only files in same folder)
-# julia> @run "../file.jl" (has autocompletion)
+# Run Julia files from the REPL with the following syntax:
+# > @run "../file.jl" <-- String (any rel/abs path) [si autocompletion]
+# > @run     file.jl  <-- Expr   (no ./ or ../ etc) [no autocompletion]
+# > @run     file     <-- Symbol (no ./ or ../ etc) [no autocompletion]
 macro run(filename)
-   if typeof(filename) == Expr
-      include(repr(filename.args[1])[2:end] * ".jl")
-   elseif typeof(filename) == String
+   if typeof(filename) == String
       include(filename)
+   elseif typeof(filename) == Expr
+      include(join([string(filename.args[1]),
+                    string(filename.args[2].value)],
+                   "."))
+   elseif typeof(filename) == Symbol
+      include(string(filename) * ".jl")
+   else
+      error("The filepath must be a string.")
    end
 end
