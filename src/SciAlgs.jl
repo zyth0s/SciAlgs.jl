13a170428cf15079bdd5a6573e5dcbb3dfaaad59
Daniel Menendez Crespo <danielmail7@gmail.com>
2020-08-11 16:04:43 +0200

    Removed magic_run.jl and cleaned SciAlgs.jl

--- src/SciAlgs.jl
+++ src/SciAlgs.jl
@@ -1,13 +1,7 @@
 module SciAlgs
 
-include("magic_run.jl")
-
 include("./Xtal/Xtal.jl")
 include("./NumQuad/NumQuad.jl")
 include("./Algorithms_from_THE_BOOK/TheBook.jl")
-
-greet() = print("Hello World!daniel")
-
-#SciAlgs.Xtal.example1()
 
 end # module
