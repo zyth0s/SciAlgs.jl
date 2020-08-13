cb5ad9c45970c8d59559afc9edbf84946606da9c
Daniel Menendez Crespo <danielmail7@gmail.com>
2020-08-13 15:57:46 +0200

    Fixed test of peasant product.

--- test/test_peasantproduct.jl
+++ test/test_peasantproduct.jl
@@ -4,7 +4,7 @@ using SciAlgs.TheBook: peasantproduct
 @testset "THE BOOK: Peasant product" begin
 
    @test  peasantproduct(10, 33) == 10 * 33
-   a, b = rand.(Int, 2)
+   a, b = rand.(UInt, 2)
    @test  peasantproduct(a, b) == a * b
 end
 
