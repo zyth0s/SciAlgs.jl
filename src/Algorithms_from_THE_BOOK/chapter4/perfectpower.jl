79672ac9219070a85b52746ea6fb20a8cf0ac836
Daniel Menendez Crespo <danielmail7@gmail.com>
2020-08-23 19:29:59 +0200

    Tested perfect power test.

--- src/Algorithms_from_THE_BOOK/chapter4/perfectpower.jl
+++ src/Algorithms_from_THE_BOOK/chapter4/perfectpower.jl
@@ -1,18 +1,6 @@
 # Author: Kenneth Lange @ University of California, Los Angeles 
 
-"""Delivers all primes <= n."""
-function eratosthenes(n::Integer)
-  isprime = trues(n)
-  isprime[1] = false # 1 is composite
-  for i = 2:round(Int, sqrt(n))
-    if isprime[i]
-      for j = i^2:i:n # all multiples of i < i^2 already composite
-        isprime[j] = false
-      end
-    end
-  end
-  filter(x -> isprime[x], 1:n) # eliminate composite numbers
-end
+import SciAlgs.TheBook: eratosthenes
 
 """Determines whether n is a perfect power."""
 function perfectpower(n::Integer)
@@ -26,6 +14,3 @@ function perfectpower(n::Integer)
   end
   false
 end
-
-perfectpower(1000)
-
---
+++ test/test_perfectpower.jl
@@ -0,0 +1,8 @@
+
+using SciAlgs.TheBook: perfectpower
+
+@testset "THE BOOK: Perfect power" begin
+
+   @test perfectpower(1000) == true
+end
+
